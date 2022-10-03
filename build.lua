a79cca4042ed480bc546e046cc384459007e942c
Yegor Bugayenko <yegor256@gmail.com>
2022-10-03 09:10:17 +0300

    #11 depends

--- build.lua
+++ build.lua
@@ -2,6 +2,8 @@ module = "crumbs"
 ctanupload = true
 typesetopts = "--interaction=batchmode --shell-escape"
 checkopts = "--interaction=batchmode --shell-escape"
+tagfiles = {"build.lua", "iexec.dtx"}
+dynamicfiles = {"*.crumbs"}
 
 uploadconfig = {
   pkg = "crumbs",
@@ -13,21 +15,19 @@ uploadconfig = {
   announcement = "Some non-critical bug fixes",
   ctanPath = "/macros/latex/contrib/crumbs",
   bugtracker = "https://github.com/yegor256/crumbs/issues",
-  home = "https://github.com/yegor256/crumbs",
+  home = "",
   description = "This package adds a navigation path (“breadcrumb trail”) at the header of a presentation, just like some websites do in order to simplify navigation.",
-  development = "https://github.com/yegor256/crumbs",
+  development = "",
   license = "mit",
   summary = "Add a Navigation Path to the page header",
   repository = "https://github.com/yegor256/crumbs",
-  support = "https://github.com/yegor256/crumbs",
+  support = "",
   topic = "headings"
 }
 
-function update_tag (file , content , tagname , tagdate)
-  if string.match(file, "%.dtx$") then
-    return string
-      .gsub(content, "0%.0%.0", tagname)
-      .gsub(content, "00%.00%.0000", os.date("%d.%m.%Y"))
-  end
-  return content
+function update_tag(file, content, tagname, tagdate)
+  return string.gsub(
+    string.gsub(content, "0%.0%.0", tagname),
+    "00%.00%.0000", os.date("%d.%m.%Y")
+  )
 end
