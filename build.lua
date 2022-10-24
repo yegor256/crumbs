f83eecb3be080255a6a4fe7c6d925691dd730a58
Yegor Bugayenko <yegor256@gmail.com>
2022-10-24 10:51:26 +0200

    -halt-on-error

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 module = "crumbs"
 ctanupload = true
-typesetopts = "--interaction=batchmode --shell-escape"
-checkopts = "--interaction=batchmode --shell-escape"
+typesetopts = "-interaction=batchmode -shell-escape -halt-on-error"
+checkopts = "-interaction=batchmode -shell-escape -halt-on-error"
 tagfiles = {"build.lua", "crumbs.dtx"}
 dynamicfiles = {"*.crumbs"}
 
