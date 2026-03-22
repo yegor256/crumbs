48206a447ce5b01dd6e23e95abff1df8cf48f0e0
Yegor Bugayenko <yegor256@gmail.com>
2026-03-22 18:14:27 +0300

    version with date

--- build.lua
+++ build.lua
@@ -10,7 +10,7 @@ dynamicfiles = {"*.crumbs"}
 
 uploadconfig = {
   pkg = "crumbs",
-  version = "0.0.0",
+  version = "0.0.0 0000/00/00",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
